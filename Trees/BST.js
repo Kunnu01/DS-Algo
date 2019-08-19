9c1b644906d8b032afc17954f3b18377387c9870
kunnu01 <kunalbhatia2409@gmail.com>
2019-08-19 16:32:33 +0530

    add BST

---
+++ Trees/BST.js
@@ -0,0 +1,70 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const node = new Node(value);
+        if (!this.root) {
+            this.root = node;
+            return this;
+        } else {
+            let current = this.root;
+            while (true) {
+                if (value === current.value) {
+                    return undefined; // or node count can be incremented
+                }
+                if (value < current.value) {
+                    if (!current.left) {
+                        current.left = node;
+                        return this;
+                    }
+                    current = current.left;
+                } else {
+                    if (!current.right) {
+                        current.right = node;
+                        return this;
+                    }
+                    current = current.right;
+                }
+            }
+        }
+    }
+
+    find(value) {
+        if (!this.root) {
+            return false;
+        }
+
+        let current = this.root;
+        while (current) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+const tree = new BinarySearchTree();
+
+tree.insert(10);
+tree.insert(2);
+tree.insert(1);
+tree.insert(20);
+tree.insert(15);
+
+console.log(tree.find(25)); // false;
